1eeb8e3d7f6067b99c846ac6efac077b9c6508b9
Josh Borrow <josh@joshborrow.com>
2026-02-23 16:23:04 -0500

    Improve rendering for slack notifications

--- datadotmd/system/notifications.py
+++ datadotmd/system/notifications.py
@@ -11,7 +11,7 @@ def notify_new_data_md_file(update_time: datetime, new_content: str, path: str):
         message=(
             f"*New Data Description for {path}*\n"
             f"> Data description created for {path}\n"
-            f"> Time of update: {update_time.strftime('%Y-%m-%d %H:%M:%S')}\n"
+            f"> Time of update: _{update_time.strftime('%Y-%m-%d %H:%M:%S')}_\n"
             f"> View this new data description at:\n"
             f"> {settings.app_base_url}/browse/{path}"
         ),
@@ -24,7 +24,7 @@ def notify_changed_data_md_file(update_time: datetime, new_content: str, path: s
         message=(
             f"*Data Description Updated for {path}*\n"
             f"> Data description updated for {path}\n"
-            f"> Time of update: {update_time.strftime('%Y-%m-%d %H:%M:%S')}\n"
+            f"> Time of update: _{update_time.strftime('%Y-%m-%d %H:%M:%S')}_\n"
             f"> View this new data description at:\n"
             f"> {settings.app_base_url}/browse/{path}"
         ),
@@ -36,7 +36,7 @@ def notify_data_updated(update_time: datetime, path: str):
         message=(
             f"*Underlying Data Updated at {path}*\n"
             f"> Data was updated at {path} without update to metadata\n"
-            f"> Time of update: {update_time.strftime('%Y-%m-%d %H:%M:%S')}\n\n"
+            f"> Time of update: _{update_time.strftime('%Y-%m-%d %H:%M:%S')}_\n"
             f"> View the data description at:\n"
             f"> {settings.app_base_url}/browse/{path}\n"
             "> _Please consider updating it to match the new data_"
